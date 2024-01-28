d5dce7624660a44e9be4e817ad0078ca3794bd8b
Oliver Cook <olliewatford@gmail.com>
2024-01-28 17:52:41 +0000

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <p className="absolute bottom-2 left-1/2 -translate-x-1/2 text-sm">Built by <a href="https://www.olliecookie.com" className="underline" target="_blank">Ollie Cook</a>&#x1f36a;</p>
+        <Analytics />
       </body>
     </html>
   );
